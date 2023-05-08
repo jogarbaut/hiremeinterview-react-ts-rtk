75f61419b9b4cd777fd831415fceb777b98dc408
Jomel Bautista <jomelgbautista@gmail.com>
2023-05-07 21:36:30 -0700

    updated default questions

--- src/data/questionSets.ts
+++ src/data/questionSets.ts
@@ -1,37 +1,83 @@
 export const questionSets = [
   {
     id: "1",
-    title: "Basic Interview Questions",
+    title: "Most Common Interview Questions",
     isFavorite: false,
     isCustom: false,
     questions: [
       {
-        id: "11",
+        id: "1",
         question: "Tell me about yourself.",
         isFavorite: false,
         isCustom: false,
       },
       {
-        id: "12",
-        question: "What are your strengths?",
+        id: "2",
+        question:
+          "Why are you applying for this position?",
         isFavorite: false,
         isCustom: false,
       },
       {
-        id: "13",
-        question: "What are your weaknesses?",
+        id: "3",
+        question:
+          "Walk me through your resume.",
         isFavorite: false,
         isCustom: false,
       },
       {
-        id: "14",
-        question: "Why do you want this job?",
+        id: "4",
+        question:
+          "Tell me about your strengths.",
         isFavorite: false,
         isCustom: false,
       },
       {
-        id: "15",
-        question: "What are your short-term and long-term career goals?",
+        id: "5",
+        question:
+          "Tell me about your weaknesses.",
+        isFavorite: false,
+        isCustom: false,
+      },
+      {
+        id: "6",
+        question:
+          "What are the most important things you are looking for in your next role?",
+        isFavorite: false,
+        isCustom: false,
+      },
+      {
+        id: "7",
+        question:
+          "Tell me about a challenge or conflict you've faced at work, and how you dealt with it.",
+        isFavorite: false,
+        isCustom: false,
+      },
+      {
+        id: "8",
+        question:
+          "Where do you see yourself in 5 years?",
+        isFavorite: false,
+        isCustom: false,
+      },
+      {
+        id: "9",
+        question:
+          "What made you apply to this company?",
+        isFavorite: false,
+        isCustom: false,
+      },
+      {
+        id: "10",
+        question:
+          "When can you start?",
+        isFavorite: false,
+        isCustom: false,
+      },
+      {
+        id: "11",
+        question:
+          "Do you have any questions for me (the interviewer)?",
         isFavorite: false,
         isCustom: false,
       },
@@ -39,46 +85,49 @@ export const questionSets = [
   },
   {
     id: "2",
-    title: "Common Behavioral Questions",
+    title: "Internship",
     isFavorite: false,
     isCustom: false,
     questions: [
       {
-        id: "21",
-        question: "Can you describe a time when your work was criticized?",
+        id: "1",
+        question: "Tell me about yourself.",
         isFavorite: false,
         isCustom: false,
       },
       {
-        id: "22",
-        question:
-          "Have you ever been on a team where someone was not completing their work? How did you handle the situation?",
+        id: "2",
+        question: "Why are you interested in this internship?",
         isFavorite: false,
         isCustom: false,
       },
       {
-        id: "23",
-        question: "Describe your method for keeping track of multiple tasks.",
+        id: "3",
+        question: "What skills and experience do you hope to gain during your time as an intern?",
         isFavorite: false,
         isCustom: false,
       },
       {
-        id: "24",
-        question:
-          "Describe a significant challenge you have experience in your career or education. How did you overcome it?",
+        id: "4",
+        question: "Can you tell me about a previous internship or work experience that has prepared you for this role??",
         isFavorite: false,
         isCustom: false,
       },
       {
-        id: "25",
-        question:
-          "How would you approach an assigned task if you were unsure on how to approach it?",
+        id: "5",
+        question: "How do you approach working in a team environment??",
         isFavorite: false,
         isCustom: false,
       },
       {
-        id: "26",
-        question: "How do you prioritize projects under pressure?",
+        id: "6",
+        question: "How do you stay organized and manage your time effectively?",
+        isFavorite: false,
+        isCustom: false,
+      },
+      {
+        id: "7",
+        question: "Can you tell me about a time when you demonstrated strong communication skills in a professional setting?",
         isFavorite: false,
         isCustom: false,
       },
@@ -86,27 +135,46 @@ export const questionSets = [
   },
   {
     id: "3",
-    title: "Questions about leadership styles",
+    title: "Personal Background and Experience",
     isFavorite: false,
     isCustom: false,
     questions: [
       {
-        id: "987",
-        question: "How would you define your leadership style?",
+        id: "1",
+        question: "Tell me about your background and how it has prepared you for this role.",
         isFavorite: false,
         isCustom: false,
       },
       {
-        id: "654",
+        id: "2",
         question:
-          "What would you do if a team member was falling behind in meeting their deadlines?",
+          "What motivates you to pursue this career?",
         isFavorite: false,
         isCustom: false,
       },
       {
-        id: "357",
+        id: "3",
+        question: "What have been some of your most significant accomplishments thus far?",
+        isFavorite: false,
+        isCustom: false,
+      },
+      {
+        id: "4",
         question:
-          "How would you handle a manager asking you to complete work outside of your defined scope?",
+          "Can you tell me about a challenge or obstacle you've faced in your personal or professional life and how you overcame it?",
+        isFavorite: false,
+        isCustom: false,
+      },
+      {
+        id: "5",
+        question:
+          "How would you describe yourself in three words?",
+        isFavorite: false,
+        isCustom: false,
+      },
+      {
+        id: "6",
+        question: "Can you tell me about a time you received constructive feedback, and how you responded to it?",
         isFavorite: false,
         isCustom: false,
       },
@@ -114,27 +182,48 @@ export const questionSets = [
   },
   {
     id: "4",
-    title: "Questions about leadership styles",
+    title: "Education and Qualifications",
     isFavorite: false,
     isCustom: false,
     questions: [
       {
-        id: "987",
-        question: "How would you define your leadership style?",
+        id: "1",
+        question: "What skills or knowledge have you gained through your education or training that are relevant to this role?",
         isFavorite: false,
         isCustom: false,
       },
       {
-        id: "654",
+        id: "2",
         question:
-          "What would you do if a team member was falling behind in meeting their deadlines?",
+          "Why did you choose your particular field of study, and how does it relate to this job?",
         isFavorite: false,
         isCustom: false,
       },
       {
-        id: "357",
+        id: "3",
         question:
-          "How would you handle a manager asking you to complete work outside of your defined scope?",
+          "Can you tell me about any certifications or licenses you have obtained and how they relate to this position?",
+        isFavorite: false,
+        isCustom: false,
+      },
+      {
+        id: "4",
+        question:
+          "What have you learned from your previous internships or work experience that will help you succeed in this role?",
+        isFavorite: false,
+        isCustom: false,
+      },
+      {
+        id: "5",
+        question:
+          "How do you stay up-to-date with industry developments and new technologies?",
+        isFavorite: false,
+        isCustom: false,
+      },
+      {
+        id: "6",
+        question:
+          "What are your future education or professional development goals?",
         isFavorite: false,
         isCustom: false,
       },
@@ -142,27 +231,55 @@ export const questionSets = [
   },
   {
     id: "5",
-    title: "Questions about leadership styles",
+    title: "Career Goals and Aspirations",
     isFavorite: false,
     isCustom: false,
     questions: [
       {
-        id: "987",
-        question: "How would you define your leadership style?",
+        id: "1",
+        question: "Where do you see yourself in five years?",
         isFavorite: false,
         isCustom: false,
       },
       {
-        id: "654",
+        id: "2",
         question:
-          "What would you do if a team member was falling behind in meeting their deadlines?",
+          "What are your long-term career goals?",
         isFavorite: false,
         isCustom: false,
       },
       {
-        id: "357",
+        id: "3",
         question:
-          "How would you handle a manager asking you to complete work outside of your defined scope?",
+          "How does this position fit into your overall career aspirations?",
+        isFavorite: false,
+        isCustom: false,
+      },
+      {
+        id: "4",
+        question:
+          "What are your long-term career goals?",
+        isFavorite: false,
+        isCustom: false,
+      },
+      {
+        id: "5",
+        question:
+          "What are you hoping to gain from this role?",
+        isFavorite: false,
+        isCustom: false,
+      },
+      {
+        id: "6",
+        question:
+          "Can you tell me about a time when you set a goal for yourself and what steps you took to achieve it?",
+        isFavorite: false,
+        isCustom: false,
+      },
+      {
+        id: "7",
+        question:
+          "How do you prioritize your professional goals?",
         isFavorite: false,
         isCustom: false,
       },
@@ -170,27 +287,258 @@ export const questionSets = [
   },
   {
     id: "6",
-    title: "Questions about leadership styles",
+    title: "Technical Skills and Knowledge",
     isFavorite: false,
     isCustom: false,
     questions: [
       {
-        id: "987",
-        question: "How would you define your leadership style?",
+        id: "1",
+        question: "Can you tell me about your experience using specific software or tools required for this role?",
         isFavorite: false,
         isCustom: false,
       },
       {
-        id: "654",
+        id: "2",
         question:
-          "What would you do if a team member was falling behind in meeting their deadlines?",
+          "How do you troubleshoot technical problems?",
         isFavorite: false,
         isCustom: false,
       },
       {
-        id: "357",
+        id: "3",
+        question:
+          "Can you walk me through a technical project you worked on, and what your role was in it?",
+        isFavorite: false,
+        isCustom: false,
+      },
+      {
+        id: "4",
+        question:
+          "How do you stay up-to-date with new technologies and developments in your field?",
+        isFavorite: false,
+        isCustom: false,
+      },
+      {
+        id: "5",
+        question:
+          "What technical skills do you hope to develop further in this role?",
+        isFavorite: false,
+        isCustom: false,
+      },
+    ],
+  },
+  {
+    id: "7",
+    title: "Interpersonal Skills and Teamwork",
+    isFavorite: false,
+    isCustom: false,
+    questions: [
+      {
+        id: "1",
+        question: "Tell me about a time when you had to work with a difficult coworker or supervisor, and how you handled the situation.",
+        isFavorite: false,
+        isCustom: false,
+      },
+      {
+        id: "2",
+        question:
+          "How do you handle disagreements or conflicts with coworkers or team members?",
+        isFavorite: false,
+        isCustom: false,
+      },
+      {
+        id: "3",
         question:
           "How would you handle a manager asking you to complete work outside of your defined scope?",
+        isFavorite: false,
+        isCustom: false,
+      },
+      {
+        id: "4",
+        question:
+          "How do you build strong working relationships with people you work with?",
+        isFavorite: false,
+        isCustom: false,
+      },
+      {
+        id: "5",
+        question:
+          "How do you encourage collaboration and teamwork in a group setting?",
+        isFavorite: false,
+        isCustom: false,
+      },
+      {
+        id: "6",
+        question:
+          "What is your communication style, and how do you adapt it to different situations?",
+        isFavorite: false,
+        isCustom: false,
+      },
+      {
+        id: "7",
+        question:
+          "WHow do you handle criticism or feedback from team members or supervisors?",
+        isFavorite: false,
+        isCustom: false,
+      },
+      {
+        id: "8",
+        question:
+          "How do you prioritize tasks when working with a team?",
+        isFavorite: false,
+        isCustom: false,
+      },
+    ],
+  },
+  {
+    id: "8",
+    title: "Problem-solving and Critical Thinking",
+    isFavorite: false,
+    isCustom: false,
+    questions: [
+      {
+        id: "1",
+        question: "Can you walk me through your process for solving a complex problem?",
+        isFavorite: false,
+        isCustom: false,
+      },
+      {
+        id: "2",
+        question:
+          "How do you approach a problem that you're not sure how to solve?",
+        isFavorite: false,
+        isCustom: false,
+      },
+      {
+        id: "3",
+        question:
+          "How do you determine the root cause of a problem?",
+        isFavorite: false,
+        isCustom: false,
+      },
+      {
+        id: "4",
+        question:
+          "Can you tell me about a time when you had to make a difficult decision?",
+        isFavorite: false,
+        isCustom: false,
+      },
+      {
+        id: "5",
+        question:
+          "How do you balance speed and accuracy when solving a problem?",
+        isFavorite: false,
+        isCustom: false,
+      },
+    ],
+  },
+  {
+    id: "9",
+    title: "Time Management and Organizational Skills",
+    isFavorite: false,
+    isCustom: false,
+    questions: [
+      {
+        id: "1",
+        question: "How do you manage your time when faced with multiple projects or tasks?",
+        isFavorite: false,
+        isCustom: false,
+      },
+      {
+        id: "2",
+        question:
+          "Can you tell me about a time when you had to manage competing priorities?",
+        isFavorite: false,
+        isCustom: false,
+      },
+      {
+        id: "3",
+        question:
+          "How do you stay organized, and what tools or methods do you use?",
+        isFavorite: false,
+        isCustom: false,
+      },
+      {
+        id: "4",
+        question:
+          "How do you ensure that you meet deadlines?",
+        isFavorite: false,
+        isCustom: false,
+      },
+      {
+        id: "5",
+        question:
+          "Can you give an example of a time when you had to reprioritize tasks to meet a deadline?",
+        isFavorite: false,
+        isCustom: false,
+      },
+      {
+        id: "6",
+        question:
+          "How do you plan and schedule your workday?",
+        isFavorite: false,
+        isCustom: false,
+      },
+      {
+        id: "7",
+        question:
+          "How do you handle interruptions or unexpected changes to your schedule?",
+        isFavorite: false,
+        isCustom: false,
+      },
+    ],
+  },
+  {
+    id: "10",
+    title: "Customer Service",
+    isFavorite: false,
+    isCustom: false,
+    questions: [
+      {
+        id: "1",
+        question: "How do you approach difficult or unhappy customers?",
+        isFavorite: false,
+        isCustom: false,
+      },
+      {
+        id: "2",
+        question:
+          "Can you give an example of a time when you went above and beyond to satisfy a customer's needs?",
+        isFavorite: false,
+        isCustom: false,
+      },
+      {
+        id: "3",
+        question:
+          "How do you handle customer complaints or issues?",
+        isFavorite: false,
+        isCustom: false,
+      },
+      {
+        id: "4",
+        question:
+          "How do you ensure that you meet deadlines?",
+        isFavorite: false,
+        isCustom: false,
+      },
+      {
+        id: "5",
+        question:
+          "How do you personalize your service to individual customers?",
+        isFavorite: false,
+        isCustom: false,
+      },
+      {
+        id: "6",
+        question:
+          "How do you handle high-pressure situations in a customer service setting?",
+        isFavorite: false,
+        isCustom: false,
+      },
+      {
+        id: "7",
+        question:
+          "How do you ensure that customers are satisfied with your service?",
         isFavorite: false,
         isCustom: false,
       },
